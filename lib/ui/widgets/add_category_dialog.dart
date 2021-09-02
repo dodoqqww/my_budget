6b9b549ab4764c918362d0110ab2faae9080b1cb
dodoqqww <dodoqqww@gmail.com>
2021-09-02 20:06:45 +0200

    ui update setting add category

---
+++ lib/ui/widgets/add_category_dialog.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+import 'package:my_budget/ui/common/style.dart';
+
+Future<void> addCategoryDialog(BuildContext context) async {
+  return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(25.0),
+            ),
+            title: Text('Add category'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: InkWell(
+                      child: Icon(
+                        Icons.circle,
+                        size: 32,
+                      ),
+                      onTap: () {
+                        openColorPicker(context);
+                      },
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    child: TextField(
+                      onChanged: (value) {
+                        // setState(() {
+                        //   valueText = value;
+                        // });
+                      },
+                      //controller: _textFieldController,
+                      decoration: getAppTextFieldDecoration(labelText: "Name"),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: Icon(Icons.arrow_back)),
+                    FloatingActionButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(Icons.save),
+                    )
+                  ],
+                ),
+              ),
+            ]
+                // actions: <Widget>[
+//
+                //   // ignore: deprecated_member_use
+                //   FloatingActionButton(onPressed: (){},child: Icon(Icons.save),),
+                // ],
+                ));
+      });
+}
+
+void openColorPicker(BuildContext context) async {
+  showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          contentPadding: const EdgeInsets.all(6.0),
+          //title: Text(title),
+          content: MaterialColorPicker(
+            onColorChange: (Color color) {
+              // Handle color changes
+            },
+            selectedColor: Colors.red,
+            colors: [
+              Colors.red,
+              Colors.deepOrange,
+              Colors.yellow,
+              Colors.lightGreen
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: Text('CANCEL'),
+              onPressed: Navigator.of(context).pop,
+            ),
+            TextButton(
+              child: Text('SUBMIT'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                //   setState(() => _mainColor = _tempMainColor);
+                //   setState(() => _shadeColor = _tempShadeColor);
+              },
+            ),
+          ],
+        );
+      });
+}
